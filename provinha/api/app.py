ed191a7f432403052fdfe3ed7cd9140a5a2a673d
johnny de Mendonça Pereira <jmtdbg@gmail.com>
2021-10-25 09:21:17 -0400

    feat:sql_alchemy

--- provinha/api/app.py
+++ provinha/api/app.py
@@ -1,15 +1,27 @@
 from flask import Flask
 from flask_restful import Api
 from resources.residencia import Residencias, Residencia
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+# caminho e configuração do banco
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 api = Api(app)
+CORS(app)
 
+
+# cria banco
+@app.before_first_request
+def cria_banco():
+    banco.create_all()
 
 # adicionando os recursos
 api.add_resource(Residencias, '/residencias')
-api.add_resource(Residencia, '/residencias/<int:residencia_id>')
+api.add_resource(Residencia, '/residencias/<int:id>')
 
 if __name__ == '__main__':
+    from sql_alchemy import banco
+    banco.init_app(app)
     app.run(debug=True)
--- provinha/api/models/residencia.py
+++ provinha/api/models/residencia.py
@@ -1,8 +1,27 @@
+from sql_alchemy import banco
 
+class ResidenciaModel(banco.Model):
+    __tablename__ = 'residencias'
 
-class ResidenciaModel:
+    id = banco.Column(banco.Integer, primary_key=True)
+    name = banco.Column(banco.String(80))
+    host_id = banco.Column(banco.String(20))
+    host_name = banco.Column(banco.String(80))
+    neighbourhood_group = banco.Column(banco.String(80))
+    neighbourhood = banco.Column(banco.String(80))
+    latitude = banco.Column(banco.Float)
+    longitude = banco.Column(banco.Float)
+    room_type = banco.Column(banco.String(80))
+    price = banco.Column(banco.Float)
+    minimum_nights = banco.Column(banco.String(20))
+    number_of_reviews = banco.Column(banco.String(20))
+    last_review = banco.Column(banco.String(80))
+    reviews_per_month = banco.Column(banco.Float)
+    calculated_host_listings_count = banco.Column(banco.String(20))
+    availability_365 = banco.Column(banco.String(20))
+    
     def __init__(self, id, name, host_id, host_name, neighbourhood_group, neighbourhood, latitude, longitude, room_type, price, minimum_nights, number_of_reviews, last_review, reviews_per_month, calculated_host_listings_count, availability_365):
-        self.residencia_id = id
+        self.id = id
         self.name = name
         self.host_id = host_id
         self.host_name = host_name
@@ -21,7 +40,7 @@ class ResidenciaModel:
 
     def json(self):
         return {
-            'id': self.residencia_id,  
+            'id': self.id,  
             'name': self.name,
             'host_id': self.host_id,
             'host_name': self.host_name,
@@ -38,3 +57,35 @@ class ResidenciaModel:
             'calculated_host_listings_count': self.calculated_host_listings_count,
             'availability_365': self.availability_365
         }
+    @classmethod
+    def find_residencia(cls, id):
+        # SELECT * FROM residencias WHERE id = $id LIMIT 1;
+        residencia = cls.query.filter_by(id=id).first()
+        if residencia:
+            return residencia
+        return None
+    
+    def save_residencia(self):
+        banco.session.add(self)
+        banco.session.commit()
+
+    def update_residencia(self, name, host_id, host_name, neighbourhood_group, neighbourhood, latitude, longitude, room_type, price, minimum_nights, number_of_reviews, last_review, reviews_per_month, calculated_host_listings_count, availability_365):
+        self.name = name
+        self.host_id = host_id
+        self.host_name = host_name
+        self.neighbourhood_group = neighbourhood_group
+        self.neighbourhood = neighbourhood
+        self.latitude = latitude
+        self.longitude = longitude
+        self.room_type = room_type
+        self.price = price
+        self.minimum_nights = minimum_nights
+        self.number_of_reviews = number_of_reviews
+        self.last_review = last_review
+        self.reviews_per_month = reviews_per_month
+        self.calculated_host_listings_count = calculated_host_listings_count
+        self.availability_365 = availability_365
+    
+    def delete_residencia(self):
+        banco.session.delete(self)
+        banco.session.commit()
